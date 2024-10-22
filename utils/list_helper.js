fba49cc95102d5eca83e7fa487a4e4adc704b321
Alejandro <aperalva@gmail.com>
2024-10-22 22:26:53 +0100

    4.5*: Funciones Auxiliares y Pruebas Unitarias, paso 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,18 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
   }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+      return null
+    }
+
+    return blogs.reduce((prev, current) => {
+      return (prev.likes > current.likes) ? prev : current
+    })
+  }
+
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
