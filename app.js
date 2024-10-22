8fa4f0e88bd476dc44f428edec378ddf2ffa2440
Alejandro <aperalva@gmail.com>
2024-10-22 19:47:56 +0100

    4.2 Lista de Blogs, paso 2

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-const blogRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const blogRouter = require('./controllers/blogs')
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
