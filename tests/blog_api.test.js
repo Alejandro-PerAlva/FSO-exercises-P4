8c9ac6320902bf5fcead01c0e537a72ddee32775
Alejandro <aperalva@gmail.com>
2024-10-23 13:30:47 +0100

    4.10: Pruebas de Lista de Blogs, paso 3

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,11 +1,10 @@
-const { test, after, beforeEach, describe } = require('node:test')
-const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const assert = require('node:assert')
 const app = require('../app')
-const api = supertest(app)
-
 const Blog = require('../models/blog')
+const api = supertest(app)
+const { test, after, beforeEach, describe } = require('node:test')
 
 const initialBlogs = [
   {
@@ -31,30 +30,44 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
+describe('POST /api/blogs', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: "New Blog Post",
+      author: "John Doe",
+      url: "http://example.com/",
+      likes: 2
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, initialBlogs.length + 1)
+    const titles = response.body.map(r => r.title)
+    assert(titles.includes('New Blog Post'))
+  })
+})
+
 describe('GET /api/blogs', () => {
   test('blogs are returned as json', async () => {
-    const response = await api
+    await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
-
-    assert.strictEqual(response.status, 200)
-    assert.match(response.headers['content-type'], /application\/json/)
   })
+})
 
-  test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
-
-    assert.strictEqual(response.body.length, initialBlogs.length)
-  })
-
-  test('the unique identifier property of the blog posts is named id', async () => {
+describe('unique identifier property is id', () => {
+  test('verifies that the unique identifier is named id', async () => {
     const response = await api.get('/api/blogs')
     const blogs = response.body
-
     blogs.forEach(blog => {
-      assert(blog.id, 'Each blog should have an id property')
-      assert.strictEqual(blog._id, undefined, 'The property _id should not exist')
+      assert.ok(blog.id)
+      assert.strictEqual(blog._id, undefined)
     })
   })
 })
